a4ced566e759e6f43fde18c08a67b98f263bbd84
Joseth David <davidguerrero777@gmail.com>
2014-11-05 08:29:34 -0500

    djStatic en wsgi

--- WissenServer/wsgi.py
+++ WissenServer/wsgi.py
@@ -12,3 +12,13 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WissenServer.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+
+"""
+Desde Heroku
+"""
+
+#from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
